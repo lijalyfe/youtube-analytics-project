a2bd5264adcfaf124bdf8910530c7923ad4371c1
lija_lyfe <lija6556@list.ru>
2023-06-07 00:29:45 +0500

    add def __init__

--- src/channel.py
+++ src/channel.py
@@ -1,11 +1,13 @@
-
+import requests
 
 class Channel:
     """Класс для ютуб-канала"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
+        self.api_key = "AIzaSyBTN9KzMhMCpC1S15uq6O9O7-xLv45AwgI"
+
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
