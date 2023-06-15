abc9b5a35a70a46ba94144bd8244710af2fc0a19
lija_lyfe <lija6556@list.ru>
2023-06-15 18:09:39 +0500

    add def __init__

--- src/video.py
+++ src/video.py
@@ -1,2 +1,7 @@
 class Video:
-
+    def __init__(self, id, title, url, views, likes)
+        self.id = id
+        self.title = title
+        self.url = url
+        self.views = views
+        self.likes = likes
