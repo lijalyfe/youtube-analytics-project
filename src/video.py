8ac790fb993e3a4b4fc0d66dcf13ad8aca095f08
lija_lyfe <lija6556@list.ru>
2023-06-15 18:11:46 +0500

    add class PLVideo(Video)

--- src/video.py
+++ src/video.py
@@ -1,7 +1,12 @@
 class Video:
-    def __init__(self, id, title, url, views, likes)
+    def __init__(self, id, title, url, views, likes):
         self.id = id
         self.title = title
         self.url = url
         self.views = views
         self.likes = likes
+
+class PLVideo(Video):
+    def __init__(self, id, title, url, views, likes, playlist_id):
+        super().__init__(id, title, url, views, likes)
+        self.playlist_id = playlist_id
