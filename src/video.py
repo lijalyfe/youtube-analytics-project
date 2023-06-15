57c18fce0849541a050bd34e31dd62fced0a9e5c
lija_lyfe <lija6556@list.ru>
2023-06-15 18:21:16 +0500

    feature def __init__ in class Video

--- src/video.py
+++ src/video.py
@@ -1,12 +1,37 @@
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+DEVELOPER_KEY = "AIzaSyBTN9KzMhMCpC1S15uq6O9O7-xLv45AwgI"
+youtube = build("youtube", "v3", developerKey=DEVELOPER_KEY)
+
 class Video:
-    def __init__(self, id, title, url, views, likes):
+    def __init__(self, id):
         self.id = id
-        self.title = title
-        self.url = url
-        self.views = views
-        self.likes = likes
+        self.title = ""
+        self.url = ""
+        self.views = 0
+        self.likes = 0
+        try:
+            video_request = youtube.videos().list(
+                part="snippet, statistics",
+                id=self.id
+            )
+            video_response = video_request.execute()
+
+            if video_response["items"]:
+                video = video_response["items"][0]
+                self.title = video["snippet"]["title"]
+                self.url = f"https://www.youtube.com/watch?v={self.id}"
+                self.views = int(video["statistics"]["viewCount"])
+                self.likes = int(video["statistics"]["likeCount"])
+
+        except HttpError as e:
+            print(f"An error occurred: {e}")
+
 
 class PLVideo(Video):
     def __init__(self, id, title, url, views, likes, playlist_id):
         super().__init__(id, title, url, views, likes)
         self.playlist_id = playlist_id
+
+
