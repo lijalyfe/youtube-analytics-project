6455f7fb24ac755fbd9479881b47bf3e45ab9335
lija_lyfe <lija6556@list.ru>
2023-06-26 01:38:50 +0500

    change class PlayList

--- src/playlist.py
+++ src/playlist.py
@@ -1,42 +1,63 @@
-import os
 import datetime
-from googleapiclient.discovery import build
-
+import requests
 
 class PlayList:
-    DEVELOPER_KEY = os.environ.get("YOUTUBE_API_KEY", "AIzaSyBTN9KzMhMCpC1S15uq6O9O7-xLv45AwgI")
+    def __init__(self, playlist_id):
+        self.playlist_id = playlist_id
+        self.title, self.url = self._get_playlist_info()
+        self.videos = self._get_videos()
 
-    def __init__(self, id):
-        self.playlist_id = id
-        self.title = ""
-        self.url = ""
-        self.videos = []
+    def _get_playlist_info(self):
+        api_key = "AIzaSyBTN9KzMhMCpC1S15uq6O9O7-xLv45AwgI"
+        url = f"https://www.googleapis.com/youtube/v3/playlists?id={self.playlist_id}&part=snippet&key={api_key}"
+        response = requests.get(url)
+        response.raise_for_status()
+        playlist = response.json()['items'][0]['snippet']
+        return playlist['title'], f"https://www.youtube.com/playlist?list={self.playlist_id}"
 
-        self.initialize_youtube()
-        self.fetch_playlist_info()
-
-    def initialize_youtube(self):
-        if self.DEVELOPER_KEY:
-            self.youtube = build('youtube', 'v3', developerKey=self.DEVELOPER_KEY)
-        else:
-            raise EnvironmentError("Youtube API key not found.")
-
-    def fetch_playlist_info(self):
-        playlist_res = self.youtube.playlists().list(
-            part='contentDetails,snippet',
-            id=self.playlist_id
-        ).execute()
-        self.title = playlist_res['items'][0]['snippet']['title']
-        self.url = f"https://www.youtube.com/playlist?list={self.playlist_id}"
+    def _get_videos(self):
+        api_key = "AIzaSyBTN9KzMhMCpC1S15uq6O9O7-xLv45AwgI"
+        url = f"https://www.googleapis.com/youtube/v3/playlistItems?playlistId={self.playlist_id}&part=snippet&maxResults=50&key={api_key}"
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()['items']
 
     @property
     def total_duration(self):
-        total = datetime.timedelta(seconds=0)
+        total_seconds = 0
         for video in self.videos:
-            duration = datetime.timedelta(seconds=int(video.duration))
-            total += duration
-        return total
+            video_id = video['snippet']['resourceId']['videoId']
+            video_response = self._get_video_details(video_id, 'contentDetails')
+            duration = video_response['items'][0]['contentDetails']['duration']
+            time_parts = duration[2:].split('M')
+            minutes = int(time_parts[0]) if time_parts[0] else 0  # Исправление ошибки
+            time_parts = time_parts[1].split('S')
+            seconds = int(time_parts[0]) if time_parts[0] else 0  # Исправление ошибки
+            total_seconds += minutes * 60 + seconds
+        return datetime.timedelta(seconds=total_seconds)
 
     def show_best_video(self):
-        best_video = max(self.videos, key=lambda x: x.likes)
-        return best_video.url
+        api_key = "AIzaSyBTN9KzMhMCpC1S15uq6O9O7-xLv45AwgI"
+        max_liked = 0
+        most_liked_video_id = ""
+        for video in self.videos:
+            video_id = video['snippet']['resourceId']['videoId']
+
+            # Получаем статистику видео по его ID
+            url = f"https://www.googleapis.com/youtube/v3/videos?id={video_id}&part=statistics&key={api_key}"
+            response = requests.get(url)
+            response.raise_for_status()
+            statistics = response.json().get('items')[0].get('statistics')
+            likes_count = statistics.get('likeCount') if 'likeCount' in statistics else 0
+
+            print(f'{video.get("snippet", {}).get("title", "NO TITLE")} - likes: {likes_count}')
+            if int(likes_count) > max_liked:
+                most_liked_video_id = video_id
+                max_liked = int(likes_count)
+        return f'https://youtu.be/{most_liked_video_id}'
+
+    def _get_video_details(self, video_id, part):
+        url = f"https://www.googleapis.com/youtube/v3/videos?id={video_id}&part={part}&key=AIzaSyBTN9KzMhMCpC1S15uq6O9O7-xLv45AwgI"
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
